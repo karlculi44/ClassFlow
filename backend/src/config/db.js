2f101be8264f57891a12bc401c0081657cdad928
karlculi44 <kaimax187@gmail.com>
2026-09-16 11:34:34 +0800

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT || 3306,
+  port: Number(process.env.DB_PORT || 3306),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
